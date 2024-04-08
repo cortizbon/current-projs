14a9d76c231bc93d944860bbbb8eb57cfcb93db6
Carlos Ortiz <carlosortiz@192.168.0.6>
2024-04-08 01:01:42 -0500

    updating info about 2025

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 import pywaffle as pwf
 from io import BytesIO
 
+from utils import create_dataframe_sankey
+
 DIC_COLORES = {'verde':["#009966"],
                'ro_am_na':["#FFE9C5", "#F7B261","#D8841C", "#dd722a","#C24C31", "#BC3B26"],
                'az_verd': ["#CBECEF", "#81D3CD", "#0FB7B3", "#009999"],
@@ -14,9 +16,11 @@ DIC_COLORES = {'verde':["#009966"],
 st.set_page_config(layout='wide')
 st.title('PePE desagregado')
 
-
-df= pd.read_csv('datasets/datos_desagregados_2019_2024.csv')
+"#F9F9F9" "#FFE9C5"
+df = pd.read_csv('datasets/datos_desagregados_2019_2024.csv')
 df['Apropiación en precios corrientes (cifras en miles de millones de pesos)'] = (df['Apropiación en precios corrientes'] /  1000_000_000).round(2)
+
+df2 = pd.read_csv('datasets/anteproyecto_2025.csv')
 sectors = df['Sector'].unique()
 entities = df['Entidad'].unique()
 tipo_gastos = df['Tipo de gasto'].unique()
@@ -24,7 +28,12 @@ cuentas = df['Cuenta'].dropna().unique()
 subcuentas = df['Subcuenta'].dropna().unique()
 projects = df['Objeto/proyecto'].dropna().unique()
 
-tab1, tab2, tab3 = st.tabs(['PEPE desagregado', 'Treemap', 'Descarga de datos'])
+tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(['PEPE desagregado', 
+                                        'Treemap', 
+                                        'Descarga de datos', 
+                                        'SankeyDiagrams',
+                                        "Lollipop",
+                                        "Anteproyecto - 2025"])
 
 with tab1:
     # cambio porcentual general
@@ -43,15 +52,6 @@ with tab1:
     with col3:
         st.metric("Variación anualizada", f"{round(change * 100, 2)}%")
 
-    piv = df.pivot_table(index='Sector',
-                         columns='Año',
-                         values='Apropiación en precios corrientes',
-                         aggfunc='sum')
-    piv['diff'] = piv[2024] - piv[2019]
-    fig, ax = plt.subplots(1, 1)
-    piv.sort_values(by='diff').tail(5)['diff'].plot(kind='barh',
-                                                    ax=ax)
-    st.pyplot(fig)
     # cambio porcentual por sector
 
     sector = st.selectbox("Seleccione un sector: ", sectors)
@@ -71,15 +71,6 @@ with tab1:
     with col3:
         st.metric(f"Variación anualizada {sector}", f"{round(change * 100, 2)}%")
 
-    piv = f_s.pivot_table(index='Entidad',
-                         columns='Año',
-                         values='Apropiación en precios corrientes',
-                         aggfunc='sum')
-    piv['diff'] = piv[2024] - piv[2019]
-    fig, ax = plt.subplots(1, 1)
-    piv.sort_values(by='diff').tail(5)['diff'].plot(kind='barh',
-                                                    ax=ax)
-    st.pyplot(fig)
 
     # cambio porcentual por entidad
     entidad = st.selectbox("Seleccione una entidad: ", f_s['Entidad'].unique())
@@ -99,15 +90,6 @@ with tab1:
     with col3:
         st.metric("Variación anualizada ", f"{round(change * 100, 2)}%")
 
-    piv = f_s_e.pivot_table(index='Tipo de gasto',
-                         columns='Año',
-                         values='Apropiación en precios corrientes',
-                         aggfunc='sum')
-    piv['diff'] = piv[2024] - piv[2019]
-    fig, ax = plt.subplots(1, 1)
-    piv.sort_values(by='diff').tail(5)['diff'].plot(kind='barh',
-                                                    ax=ax)
-    st.pyplot(fig)
 
     # cambio porcentual por tipo de gasto
 
@@ -128,15 +110,6 @@ with tab1:
     with col3:
         st.metric("Variación anualizada", f"{round(change * 100, 2)}%")
     
-    piv = f_s_e_tg.pivot_table(index='Cuenta',
-                         columns='Año',
-                         values='Apropiación en precios corrientes',
-                         aggfunc='sum')
-    piv['diff'] = piv[2024] - piv[2019]
-    fig, ax = plt.subplots(1, 1)
-    piv.sort_values(by='diff').tail(5)['diff'].plot(kind='barh',
-                                                    ax=ax)
-    st.pyplot(fig)
     # cambio porcentual por cuenta
 
     cuenta = st.selectbox("Seleccione una cuenta: ", f_s_e_tg['Cuenta'].unique())
@@ -156,16 +129,6 @@ with tab1:
     with col3:
         st.metric("Variación anualizada", f"{round(change * 100, 2)}%")
 
-    piv = f_s_e_tg_c.pivot_table(index='Subcuenta',
-                         columns='Año',
-                         values='Apropiación en precios corrientes',
-                         aggfunc='sum')
-    piv['diff'] = piv[2024] - piv[2019]
-    fig, ax = plt.subplots(1, 1)
-    piv.sort_values(by='diff').tail(5)['diff'].plot(kind='barh',
-                                                    ax=ax)
-    st.pyplot(fig)
-
     # subcuentas
     
     subcuenta = st.selectbox("Seleccione una subcuenta: ", f_s_e_tg_c['Subcuenta'].unique())
@@ -184,16 +147,6 @@ with tab1:
 
     with col3:
         st.metric("Variación anualizada", f"{round(change * 100, 2)}%")
-
-    piv = f_s_e_tg_c_sc.pivot_table(index='Objeto/proyecto',
-                         columns='Año',
-                         values='Apropiación en precios corrientes',
-                         aggfunc='sum')
-    piv['diff'] = piv[2024] - piv[2019]
-    fig, ax = plt.subplots(1, 1)
-    piv.sort_values(by='diff').tail(5)['diff'].plot(kind='barh',
-                                                    ax=ax)
-    st.pyplot(fig)
 
     # objeto
         
@@ -263,6 +216,171 @@ with tab3:
     st.download_button(label = 'Descargar datos completos',
                     data = binary_output.getvalue(),
                     file_name = 'datos_desagregados_2019_2024.xlsx')
+    
+import plotly.graph_objects as go
+with tab4:
+    fig  = go.Figure(data=go.Sankey(
+
+    ))
+
+with tab5:
+    st.header("Cambio 2019 - 2024")
+    st.subheader("Cambio por sector")
+    data = df.pivot_table(index='Sector',
+               columns='Año',
+               values='Apropiación en precios corrientes',
+               aggfunc='sum').sort_values(by=2024).tail(15).div(1_000_000_000).round(2).reset_index()
+
+    my_range = range(1, len(data['Sector']) + 1)
+
+    trace1 = go.Scatter(
+        x=data[2024],
+        y=list(my_range),
+        mode='markers',
+        name='2024',
+        hovertext=data['Sector'],
+        hoverinfo='text+x',
+        marker_color="#2635bf",
+        marker_size=10
+    )
+    trace2 = go.Scatter(
+        x=data[2019],
+        y=list(my_range),
+        mode='markers',
+        name='2019',
+        hovertext=data['Sector'],
+        hoverinfo='text+x',
+        marker_color="#D8841C",
+        marker_size=10
+    )
+
+    fig = go.Figure()
+    fig.add_trace(trace1)
+    fig.add_trace(trace2)
+
+
+    fig.update_layout(
+        title='Cambio del gasto por sector (2019 - 2024)',
+        xaxis_title='Gasto (miles de millones de pesos)',
+        yaxis_title='',
+        yaxis=dict(tickvals=list(my_range), ticktext=["" for i in my_range]),
+        xaxis_tickformat="4.",
+        xaxis_showgrid=False,
+        yaxis_showgrid=False,
+        width=1000, height=600)
+    for i, j, k in zip(my_range, data[2019], data[2024]):
+        fig.add_shape(type='line', x0=j, x1=k, y0=i, y1=i, line_color="#2635bf", line_width=0.5) 
+
+    st.plotly_chart(fig)
+
+    st.subheader("Cambio por entidad")
+    data = df.pivot_table(index='Entidad',
+               columns='Año',
+               values='Apropiación en precios corrientes',
+               aggfunc='sum').sort_values(by=2024).dropna().tail(15).div(1_000_000_000).round(2).reset_index()
+
+    my_range = range(1, len(data['Entidad']) + 1)
+
+    trace1 = go.Scatter(
+        x=data[2024],
+        y=list(my_range),
+        mode='markers',
+        name='2024',
+        hovertext=data['Entidad'],
+        hoverinfo='text+x',
+        marker_color="#2635bf",
+        marker_size=10
+    )
+    
+    trace2 = go.Scatter(
+        x=data[2019],
+        y=list(my_range),
+        mode='markers',
+        name='2019',
+        hovertext=data['Entidad'],
+        hoverinfo='text+x',
+        marker_color="#D8841C",
+        marker_size=10
+    )
+
+    fig = go.Figure()
+    fig.add_trace(trace1)
+    fig.add_trace(trace2)
+
+
+    fig.update_layout(
+        title='Cambio del gasto por entidad (2019 - 2024)',
+        xaxis_title='Gasto (miles de millones de pesos)',
+        yaxis_title='',
+        yaxis=dict(tickvals=list(my_range), ticktext=["" for i in my_range]),
+        xaxis_tickformat="4.",
+        xaxis_showgrid=False,
+        yaxis_showgrid=False,
+        width=1000, height=600)
+    for i, j, k in zip(my_range, data[2019], data[2024]):
+        fig.add_shape(type='line', x0=j, x1=k, y0=i, y1=i, line_color="#2635bf", line_width=0.5) 
+ 
+
+    st.plotly_chart(fig)
+
+with tab6:
+    st.header("Anteproyecto")
+
+    st.subheader("Treemap")
+
+    fig = px.treemap(df2, 
+                            path=[px.Constant('Anteproyecto'), 'ENTIDAD', 
+                                    'CONCEPTO'],
+                            values='TOTAL',
+                            title="Matriz de composición anual del Anteproyecto <br><sup>Cifras en millones de pesos</sup>",
+                            color_continuous_scale='Teal')
+            
+    fig.update_layout(width=1000, height=600)
+            
+    st.plotly_chart(fig)
+
+    st.subheader("Flujo del gasto")
+    lista = ['ENTIDAD', 'CONCEPTO']
+
+    top_10 = df2.groupby('ENTIDAD')['TOTAL'].sum().reset_index().sort_values(by='TOTAL', ascending=False).head(10)['ENTIDAD']
+
+    top_10_df = df2[df2['ENTIDAD'].isin(top_10)]
+
+    rev_info, conc = create_dataframe_sankey(top_10_df, 'TOTAL',*lista)
+    fig = go.Figure(data=[go.Sankey(
+    node = dict(
+      pad = 15,
+      thickness = 20,
+      line = dict(color = "#2635bf", width = 0.5),
+      label = list(rev_info.keys()),
+      color = "#2635bf"
+    ),
+    link = dict(
+      source = conc['source'], # indices correspond to labels, eg A1, A2, A1, B1, ...
+      target = conc['target'],
+      value = conc['value']
+    ))])
+
+    fig.update_layout(title_text="Flujo de gasto", font_size=10, width=1000, height=600)
+    st.plotly_chart(fig)
+
+
+
+
+    st.subheader("Descarga de datos")
+
+
+    binary_output = BytesIO()
+    df2.to_excel(binary_output, index=False)
+    st.download_button(label = 'Descargar datos de anteproyecto',
+                    data = binary_output.getvalue(),
+                    file_name = 'datos_agregados_anteproyecto_2025.xlsx')
+
+
+
+
+
+
 
 
 
---
+++ utils.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+def create_dataframe_sankey(data, value_column, *columns):
+    for col in columns:
+        if col not in data.columns:
+            raise ValueError
+    
+    groupbys = []
+    for idx, col in enumerate(columns[:-1]):
+        i = data.groupby([columns[idx], columns[idx + 1]])[value_column].sum().reset_index()
+        i.columns = ['source', 'target', 'value']
+        groupbys.append(i)
+
+    conc = pd.concat(groupbys)
+
+    info = enumerate(list(set(conc['source'].unique().tolist() + conc['target'].unique().tolist())))
+    dic_info = dict(info)
+
+    rev_info = {}
+    for key, value in dic_info.items():
+        rev_info[value] = key
+
+    conc['source'] = conc['source'].map(rev_info)
+    conc['target'] = conc['target'].map(rev_info)
+
+    return rev_info, conc
